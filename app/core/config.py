0c777f313ad4560118ee9a9ee7953679f413caf5
Andronik Maria <andronikmar@yandex.ru>
2022-07-01 14:06:29 +0300

    sef

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     app_title: str = 'Благотворительный фонд поддержки котиков'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
-    secret: str = 'SECRET'
+    secret: str = 'dsjhfcbjssdbhkbjasbhjc'
     type: Optional[str] = None
     project_id: Optional[str] = None
     private_key_id: Optional[str] = None
